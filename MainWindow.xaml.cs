3f1370c07992a27f825cd4eab2d04ee92bf93ce2
feldenserra <michael.serra1@outlook.com>
2024-03-18 14:31:51 -0700

    setup GUI and got basic writing to textbox. working on globals and setting up operations

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,5 +20,89 @@ namespace Calculator
         {
             InitializeComponent();
         }
+
+
+        private void numDisplay_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        private void num1_Click(object sender, RoutedEventArgs e)
+        {
+            operationClass.updateText(sender);
+
+        }
+        private void num2_Click(object sender, RoutedEventArgs e)
+        {
+            operationClass.updateText(sender);
+        }
+        private void num3_Click(object sender, RoutedEventArgs e)
+        {
+            operationClass.updateText(sender);
+
+        }
+        private void num4_Click(object sender, RoutedEventArgs e)
+        {
+            operationClass.updateText(sender);
+
+        }
+        private void num5_Click(object sender, RoutedEventArgs e)
+        {
+            operationClass.updateText(sender);
+
+        }
+        private void num6_Click(object sender, RoutedEventArgs e)
+        {
+            operationClass.updateText(sender);
+
+        }
+        private void num7_Click(object sender, RoutedEventArgs e)
+        {
+            operationClass.updateText(sender);
+
+        }
+        private void num8_Click(object sender, RoutedEventArgs e)
+        {
+            operationClass.updateText(sender);
+
+        }
+        private void num9_Click(object sender, RoutedEventArgs e)
+        {
+            operationClass.updateText(sender);
+
+        }
+        private void num0_Click(object sender, RoutedEventArgs e)
+        {
+            operationClass.updateText(sender);
+
+        }
+        private void numPeriod_Click(object sender, RoutedEventArgs e)
+        {
+            operationClass.putDecimal(sender);
+        }
+        private void numPlus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void numMinus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void numMultiply_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void numDivide_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void numEqual_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void numClear_Click(object sender, RoutedEventArgs e)
+        {
+            operationClass.clearText();
+        }
+
     }
 }
---
+++ operationClass.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Calculator
+{
+    public static class operationClass
+    {
+        private static TextBox? _targetDisplay;
+        private static double? _argA;
+        private static double? _argB;
+        private static bool _inCalc = false;
+
+        public static void checkNullTarget()
+        {
+            if (_targetDisplay == null)
+            {
+                _targetDisplay = Application.Current.MainWindow.FindName("numDisplay") as TextBox;
+            }
+        }
+        public static void updateText(object sender)
+        {
+            checkNullTarget();
+
+            if(sender is Button button)
+            {
+                string buttonText = button.Content.ToString();
+                if (_targetDisplay.Text == "0")
+                {
+                    _targetDisplay.Text = buttonText;
+                } else
+                {
+                _targetDisplay.Text += buttonText;
+                }
+            }
+
+        }
+        public static void putDecimal(object sender)
+        {
+            bool decimalPresent = _targetDisplay.Text.Any(s => s == '.');
+
+            if (!decimalPresent)
+            {
+                _targetDisplay.Text += ".";
+            }
+        }
+        public static void clearText()
+        {
+            checkNullTarget();
+            _targetDisplay.Text = "0";
+        }
+        public static void pressAdd(object sender)
+        {
+            checkNullTarget();
+            double _argA = double.Parse(_targetDisplay.Text);
+        }
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+    }
+}
